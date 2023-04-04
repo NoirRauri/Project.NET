b12b9199f49ddf1e856c0b02b319971ca5f59b38
Michael Alvarez <lauryyhorel@gmail.com>
2023-04-04 13:36:02 -0600

    mod

---
+++ Extra01_C#_Metodos/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Extra01_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mensaje();
+            sumarNumeros(7, 8);
+        }
+
+        static void mensaje()
+        {
+            Console.WriteLine("Nuevo, Mensaje!");
+        }
+
+        static void sumarNumeros(int num1, int num2)
+        {
+            Console.WriteLine($"La suma del numero {num1} + {num2} es {num1 + num2}");
+        }
+    }
+}
